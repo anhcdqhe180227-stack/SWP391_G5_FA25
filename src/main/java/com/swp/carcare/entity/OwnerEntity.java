88ae7b7e13492e17d3562b50c98d56bd14e9920f
lekimoanh1456@gmail.com <lekimoanh1456@gmail.com>
2025-09-23 10:03:25 +0700

    chỉnh sửa dashboard lễ tân

---
+++ src/main/java/com/swp/carcare/entity/OwnerEntity.java
@@ -0,0 +1,86 @@
+package com.swp.carcare.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "Owner")
+@Data
+public class OwnerEntity extends AbstractEntity {
+    @Column(name = "full_name")
+    private String fullName;
+
+    private String gender;
+
+    private String description;
+
+    @Basic
+    @Column(name = "phone_number", nullable = true, length = 20)
+    private String phoneNumber;
+
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    @EqualsAndHashCode.Exclude
+    @JsonBackReference
+    private UserEntity user;
+
+    @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL)
+    @EqualsAndHashCode.Exclude
+    @ToString.Exclude
+    @JsonManagedReference
+    private Set<VehicleEntity> vehicles;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public UserEntity getUser() {
+        return user;
+    }
+
+    public void setUser(UserEntity user) {
+        this.user = user;
+    }
+
+    public Set<VehicleEntity> getVehicles() {
+        return vehicles;
+    }
+
+    public void setVehicles(Set<VehicleEntity> vehicles) {
+        this.vehicles = vehicles;
+    }
+}
---
+++ src/main/java/com/swp/carcare/entity/VehicleEntity.java
@@ -0,0 +1,64 @@
+package com.swp.carcare.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Entity
+@Table(name = "Vehicle")
+@Data
+public class VehicleEntity extends AbstractEntity {
+
+    @Column(name = "model")
+    private String model;
+
+    @Column(name = "license_plate")
+    private String licensePlate;
+
+    private Integer year;
+
+    @ManyToOne
+    @JoinColumn(name = "ownerId")
+    @EqualsAndHashCode.Exclude
+    @JsonBackReference
+    private OwnerEntity owner;
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public OwnerEntity getOwner() {
+        return owner;
+    }
+
+    public void setOwner(OwnerEntity owner) {
+        this.owner = owner;
+    }
+}
