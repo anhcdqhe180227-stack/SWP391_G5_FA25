507b4206362d1f2f3a47050f0b7f7651d9f64aac
DLBTin <105615497+DLBTin@users.noreply.github.com>
2025-09-24 10:59:51 +0700

    Update carcare.sql

--- carcare.sql
+++ carcare.sql
@@ -1,106 +1,255 @@
--- Bảng Users: Chứa thông tin người dùng chung (Owner, Employee, Recep, ...)
-CREATE TABLE Users (
-    user_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    phone_number VARCHAR(20),
-    user_type ENUM('Owner', 'Employee', 'Recep') NOT NULL,  -- Phân biệt Owner, Employee, Recep
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+CREATE DATABASE  IF NOT EXISTS `carcare` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `carcare`;
+-- MySQL dump 10.13  Distrib 8.0.43, for Win64 (x86_64)
+--
+-- Host: localhost    Database: carcare
+-- ------------------------------------------------------
+-- Server version	8.0.43
 
--- Bảng Owner: Chứa user_id của Owner
-CREATE TABLE Owner (
-    owner_id INT PRIMARY KEY,
-    FOREIGN KEY (owner_id) REFERENCES Users(user_id) ON DELETE CASCADE
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Bảng Employee: Chứa user_id của Employee
-CREATE TABLE Employee (
-    emp_id INT PRIMARY KEY,
-    FOREIGN KEY (emp_id) REFERENCES Users(user_id) ON DELETE CASCADE
-);
+--
+-- Table structure for table `employee`
+--
 
--- Bảng Recep: Chứa user_id của Recep
-CREATE TABLE Recep (
-    recep_id INT PRIMARY KEY,
-    FOREIGN KEY (recep_id) REFERENCES Users(user_id) ON DELETE CASCADE
-);
+DROP TABLE IF EXISTS `employee`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `employee` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `created_date_time` datetime(6) DEFAULT NULL,
+  `created_by` varchar(255) DEFAULT NULL,
+  `status` int DEFAULT NULL,
+  `modified_date_time` datetime(6) DEFAULT NULL,
+  `updated_by` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `full_name` varchar(255) DEFAULT NULL,
+  `gender` varchar(255) DEFAULT NULL,
+  `phone_number` varchar(20) DEFAULT NULL,
+  `user_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Bảng Vehicles: Chứa thông tin về phương tiện (vehicle)
-CREATE TABLE Vehicle (
-    vehicle_id INT AUTO_INCREMENT PRIMARY KEY,
-    owner_id INT,
-    model VARCHAR(255) NOT NULL,
-    license_plate VARCHAR(20) UNIQUE NOT NULL,
-    year INT,
-    FOREIGN KEY (owner_id) REFERENCES Owner(owner_id) ON DELETE CASCADE
-);
+--
+-- Dumping data for table `employee`
+--
 
--- Bảng Parts: Chứa thông tin về các phụ tùng xe (phụ tùng có thể bị hỏng hoặc cần thay thế)
-CREATE TABLE Parts (
-    part_id INT AUTO_INCREMENT PRIMARY KEY,
-    vehicle_id INT,
-    expiration_date DATE,
-    status ENUM('Hỏng', 'OK') NOT NULL,  -- Trạng thái của phụ tùng
-    FOREIGN KEY (vehicle_id) REFERENCES Vehicle(vehicle_id) ON DELETE CASCADE
-);
+LOCK TABLES `employee` WRITE;
+/*!40000 ALTER TABLE `employee` DISABLE KEYS */;
+INSERT INTO `employee` VALUES (1,NULL,NULL,0,NULL,NULL,'Kỹ thuật sửa xe 123','Cao Quang Anh','male','09812398111',6),(2,NULL,NULL,NULL,NULL,NULL,'Làm lễ tân','Oanh Lê','female','0987111222',7);
+/*!40000 ALTER TABLE `employee` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- Bảng Schedule: Lịch làm việc của Employee
-CREATE TABLE Schedule (
-    emp_id INT,
-    schedule_date DATE,
-    shift ENUM('Morning', 'Afternoon', 'Night') NOT NULL,
-    PRIMARY KEY (emp_id, schedule_date),
-    FOREIGN KEY (emp_id) REFERENCES Employee(emp_id) ON DELETE CASCADE
-);
+--
+-- Table structure for table `logs`
+--
 
--- Bảng Logs: Ghi lại các hoạt động của người dùng (Owner, Employee, Recep, ...)
-CREATE TABLE Logs (
-    log_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT,
-    action VARCHAR(255) NOT NULL,
-    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES Users(user_id) ON DELETE CASCADE
-);
+DROP TABLE IF EXISTS `logs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `logs` (
+  `log_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int DEFAULT NULL,
+  `action` varchar(255) NOT NULL,
+  `timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`log_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `logs_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Bảng Reviews: Đánh giá của Owner đối với dịch vụ
-CREATE TABLE Review (
-    review_id INT AUTO_INCREMENT PRIMARY KEY,
-    owner_id INT,
-    rating INT NOT NULL,  -- Điểm đánh giá
-    comment TEXT,
-    FOREIGN KEY (owner_id) REFERENCES Owner(owner_id) ON DELETE CASCADE
-);
+--
+-- Dumping data for table `logs`
+--
 
--- Bảng Appointments: Cuộc hẹn giữa Owner và Employee
-CREATE TABLE Appointment (
-    appointment_id INT AUTO_INCREMENT PRIMARY KEY,
-    owner_id INT,
-    emp_id INT,
-    appointment_date DATE,
-    status ENUM('Scheduled', 'Completed', 'Cancelled') DEFAULT 'Scheduled',
-    FOREIGN KEY (owner_id) REFERENCES Owner(owner_id) ON DELETE CASCADE,
-    FOREIGN KEY (emp_id) REFERENCES Employee(emp_id) ON DELETE CASCADE
-);
+LOCK TABLES `logs` WRITE;
+/*!40000 ALTER TABLE `logs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `logs` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- Bảng AppointmentDetails: Chi tiết cuộc hẹn (dịch vụ, số lượng, giá cả, tổng tiền)
-CREATE TABLE AppointmentDetails (
-    appointment_details_id INT AUTO_INCREMENT PRIMARY KEY,
-    appointment_id INT,
-    quantity INT,
-    service VARCHAR(255) NOT NULL,
-    price DECIMAL(10, 2) NOT NULL,
-    total_price DECIMAL(10, 2) NOT NULL,
-    FOREIGN KEY (appointment_id) REFERENCES Appointment(appointment_id) ON DELETE CASCADE
-);
+--
+-- Table structure for table `owner`
+--
 
--- Bảng Invoices: Hóa đơn cho Owner sau mỗi cuộc hẹn
-CREATE TABLE Invoice (
-    invoice_id INT AUTO_INCREMENT PRIMARY KEY,
-    owner_id INT,
-    appointment_id INT,
-    total_amount DECIMAL(10, 2) NOT NULL,
-    issue_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (owner_id) REFERENCES Owner(owner_id) ON DELETE CASCADE,
-    FOREIGN KEY (appointment_id) REFERENCES Appointment(appointment_id) ON DELETE CASCADE
-);
+DROP TABLE IF EXISTS `owner`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `owner` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `created_date_time` datetime(6) DEFAULT NULL,
+  `created_by` varchar(255) DEFAULT NULL,
+  `status` int DEFAULT NULL,
+  `modified_date_time` datetime(6) DEFAULT NULL,
+  `updated_by` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `gender` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `phone_number` varchar(20) DEFAULT NULL,
+  `user_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `owner`
+--
+
+LOCK TABLES `owner` WRITE;
+/*!40000 ALTER TABLE `owner` DISABLE KEYS */;
+INSERT INTO `owner` VALUES (1,NULL,NULL,1,NULL,NULL,'12 HN','Nguyễn Đức','male','Anh','0987612345',8);
+/*!40000 ALTER TABLE `owner` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `parts`
+--
+
+DROP TABLE IF EXISTS `parts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `parts` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `created_date_time` datetime(6) DEFAULT NULL,
+  `created_by` varchar(255) DEFAULT NULL,
+  `status` int DEFAULT NULL,
+  `modified_date_time` datetime(6) DEFAULT NULL,
+  `updated_by` varchar(255) DEFAULT NULL,
+  `expiration_date` datetime(6) DEFAULT NULL,
+  `img` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `part_status` enum('BROKEN','EXCELLENT','OUT_OF_DATE') DEFAULT NULL,
+  `vehicle_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `parts`
+--
+
+LOCK TABLES `parts` WRITE;
+/*!40000 ALTER TABLE `parts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `parts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `recep`
+--
+
+DROP TABLE IF EXISTS `recep`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `recep` (
+  `recep_id` int NOT NULL,
+  PRIMARY KEY (`recep_id`),
+  CONSTRAINT `recep_ibfk_1` FOREIGN KEY (`recep_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `recep`
+--
+
+LOCK TABLES `recep` WRITE;
+/*!40000 ALTER TABLE `recep` DISABLE KEYS */;
+/*!40000 ALTER TABLE `recep` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(50) NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_date_time` datetime(6) DEFAULT NULL,
+  `created_by` varchar(255) DEFAULT NULL,
+  `status` int DEFAULT NULL,
+  `modified_date_time` datetime(6) DEFAULT NULL,
+  `updated_by` varchar(255) DEFAULT NULL,
+  `password` varchar(120) NOT NULL,
+  `role` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `UK6dotkott2kjsp8vw4d0m25fb7` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'admin@gmail.com','2025-09-21 14:09:24',NULL,NULL,1,NULL,NULL,'$2a$12$9R74XDYsxPq7i1IowWBrFOsglePyiU0ZRjyQuhYc.N5xzuWMrsnOa',0),(2,'owner@gmail.com','2025-09-21 14:09:24',NULL,NULL,1,NULL,NULL,'$2a$12$9R74XDYsxPq7i1IowWBrFOsglePyiU0ZRjyQuhYc.N5xzuWMrsnOa',1),(3,'emp@gmail.com','2025-09-21 14:09:24',NULL,NULL,1,NULL,NULL,'$2a$12$9R74XDYsxPq7i1IowWBrFOsglePyiU0ZRjyQuhYc.N5xzuWMrsnOa',2),(4,'recep@gmail.com','2025-09-21 14:09:24',NULL,NULL,1,NULL,NULL,'$2a$12$9R74XDYsxPq7i1IowWBrFOsglePyiU0ZRjyQuhYc.N5xzuWMrsnOa',3),(5,'caoquanganh@gmail.com','2025-09-23 01:58:45',NULL,NULL,1,NULL,NULL,'$2a$10$XfwLqdJ9jlXl2BRmgbo74uorr2r4nTRkTcKVwi3OHfA3l1NJLLCWG',2),(6,'quanganh@gmail.com','2025-09-23 02:04:42',NULL,NULL,1,NULL,NULL,'$2a$10$hjMNZva9hbGiOs9jm5KqyulQVj4Jo29iVhCiflvW1YJxOOVkrdUKm',2),(7,'oanhle@gmail.com','2025-09-23 02:41:14',NULL,NULL,1,NULL,NULL,'$2a$10$QeAg16nkOdd9s6WY7PaSiuQdi7WXpnoCnIa0D7b2sUukPBgeahvPO',2),(8,'chuyendizz@gmail.com','2025-09-23 07:21:26',NULL,NULL,1,NULL,NULL,'$2a$10$REvArt/bBVp3Eogbcnu77uEQMO6xgzkvplntnC41nlgPUwxbZVNC2',1);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `vehicle`
+--
+
+DROP TABLE IF EXISTS `vehicle`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `vehicle` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `created_date_time` datetime(6) DEFAULT NULL,
+  `created_by` varchar(255) DEFAULT NULL,
+  `status` int DEFAULT NULL,
+  `modified_date_time` datetime(6) DEFAULT NULL,
+  `updated_by` varchar(255) DEFAULT NULL,
+  `license_plate` varchar(255) DEFAULT NULL,
+  `model` varchar(255) DEFAULT NULL,
+  `year` int DEFAULT NULL,
+  `owner_id` bigint DEFAULT NULL,
+  `img` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKt8k6l326vbc4uim9nq1r5ug8g` (`owner_id`),
+  CONSTRAINT `FKt8k6l326vbc4uim9nq1r5ug8g` FOREIGN KEY (`owner_id`) REFERENCES `owner` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `vehicle`
+--
+
+LOCK TABLES `vehicle` WRITE;
+/*!40000 ALTER TABLE `vehicle` DISABLE KEYS */;
+INSERT INTO `vehicle` VALUES (1,NULL,'male',1,NULL,'male','30A-12345','Range Rover',2014,1,'https://s.bonbanh.com/uploads/users/482242/car/6328105/l_1752121223.898.jpg'),(2,NULL,'male',1,NULL,'male','39A-56789','Toyota Camry',2009,1,'https://s.bonbanh.com/uploads/users/592895/car/6424446/l_1757487609.949.jpg');
+/*!40000 ALTER TABLE `vehicle` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'carcare'
+--
+
+--
+-- Dumping routines for database 'carcare'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-09-24  9:29:04
